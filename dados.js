e18760973c96be65ebd0e404f25ca8a8878208ba
keltonny00 <kel4606@gmail.com>
2024-09-05 22:12:28 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+  {
+      titulo: "Gabriel Medina",
+      descricao: "Gabriel Medina é um dos maiores surfistas da história do Brasil e um dos nomes mais reconhecidos do surf mundial. Com um talento inegável e uma paixão contagiante pelo mar, Medina conquistou o mundo com suas manobras radicais e sua determinação em alcançar o topo. Tricampeão mundial, ele é um verdadeiro ídolo para os brasileiros e uma inspiração para todos aqueles que sonham em viver do surf.",
+      link: "https://pt.wikipedia.org/wiki/Gabriel_Medina"
+  },
+  {
+      titulo: "Kelly Slater",
+      descricao: "Kelly Slater é considerado um dos maiores surfistas de todos os tempos, com uma carreira repleta de títulos e recordes. Conhecido por sua técnica impecável e sua longevidade no esporte, Slater inspirou gerações de surfistas ao redor do mundo.",
+      link: "https://pt.wikipedia.org/wiki.org/wiki/Kelly_Slater"
+  },
+  {
+      titulo: "Tatiana Weston-Webb",
+      descricao: "Tatiana Weston-Webb é uma das principais surfistas brasileiras da atualidade. Com um estilo agressivo e manobras poderosas, ela conquistou o mundo do surf e se tornou um ícone para as mulheres no esporte.",
+      link: "https://pt.wikipedia.org/wiki/Tatiana_Weston-Webb"
+  }
+];
+
+    
+    
