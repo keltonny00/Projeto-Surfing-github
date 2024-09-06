953f3d3865a19524a8ca9d6bdd37de8f8cc0e12a
keltonny00 <kel4606@gmail.com>
2024-09-06 02:19:53 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log(dados);
+function pesquisar() {
+    // Obtém a seção onde os resultados serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+    console.log(section); // Loga o elemento no console para verificação
+  
+    let resultados = ""; // Inicializa uma string vazia para armazenar os resultados
+  
+    // Itera sobre cada dado na lista de dados
+    for (let dado of dados) {
+      // Cria o HTML para cada item do resultado
+      resultados += `
+        <div class="item-resultado">
+          <h2>
+            <a href="#" target="_blank">${dado.titulo}</a>
+          </h2>
+          <p class="descricao-meta">${dado.descricao}</p>
+          <a href=${dado.link} target="_blank">Mais informações</a>
+        </div>
+      `;
+    }
+  
+    // Atribui o HTML gerado à seção de resultados
+    section.innerHTML = resultados;
+  }
